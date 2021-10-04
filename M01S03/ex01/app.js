ffd0d1f7f25e5808afbbd6aabc13797186786ab5
andreiasn <andrei.asanache04@gmail.com>
2021-10-05 01:45:45 +0300

    Tema 27 septembrie

--- M01S03/ex01/app.js
+++ M01S03/ex01/app.js
@@ -13,8 +13,25 @@ console.log(
     ' ani.',
 );
 
+console.warn(`
+  Afiseaza anul in care te-ai nascut scazand varsta pe care
+  ai trecut-o in obiect din anul curent (un proces numit hardcodare)
+`);
 console.log((2021 - person.age).toString());
 
+console.warn(
+  `Afiseaza fraza “xxx s-a nascut in aaaa.” Foloseste tot anul curent pentru a scadea varsta.`,
+);
+console.log(person.name + ' s-a nascut in ' + (2021 - person.age) + '.');
+
+console.warn(`
+  Afiseaza fraza “xxx yyy are xx ani.” in
+  consola folosind operatorul de concatenare
+`);
 console.log(
   person.name + ' ' + person.surname + ' are ' + person.age + ' ani.',
 );
+
+// pt pixeltab verde:
+console.log(person.name);
+console.log(person.age.toString());
